8f2d690b61bd28cb260e02cf20dc13d5fc18a105
Akshay kumar <akshayk0552@gmail.com>
2024-07-17 14:43:30 +0530

    Redis

--- Foodie/src/main/java/com/Foodie/controller/MenuController.java
+++ Foodie/src/main/java/com/Foodie/controller/MenuController.java
@@ -5,9 +5,7 @@ import com.Foodie.model.MenuEntity;
 import com.Foodie.service.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
